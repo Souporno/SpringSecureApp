cbbee3d3a92726bcf9b592e64c39d023699cfc7c
Souporno <40350448+Souporno@users.noreply.github.com>
2021-09-13 22:12:07 +0530

    Login as per data present in H2 database. Console White label error!

--- SecureApp/src/main/java/com/example/SecureApp/AppSecurityConfig.java
+++ SecureApp/src/main/java/com/example/SecureApp/AppSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.example.SecureApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.*;
@@ -15,12 +19,24 @@ import java.util.*;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService()
+	public AuthenticationProvider authProvider()
 	{
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.withDefaultPasswordEncoder().username("Soup").password("1234").roles("USER").build());
-		return new InMemoryUserDetailsManager(users);
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
 	}
+	
+//	@Bean
+//	@Override
+//	protected UserDetailsService userDetailsService()
+//	{
+//		List<UserDetails> users = new ArrayList<>();
+//		users.add(User.withDefaultPasswordEncoder().username("Soup").password("1234").roles("USER").build());
+//		return new InMemoryUserDetailsManager(users);
+//	}
 }
