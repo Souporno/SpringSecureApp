46427c9b5d0dd6d0a6e4bd45a347fa14d75bebae
Souporno <40350448+Souporno@users.noreply.github.com>
2021-09-13 22:40:36 +0530

    Just created a JSP for login page and logout page

--- SecureApp/src/main/java/com/example/SecureApp/AppSecurityConfig.java
+++ SecureApp/src/main/java/com/example/SecureApp/AppSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -34,6 +35,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		return provider;
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
+	}
+	
+	
 	
 //	@Bean
 //	@Override
